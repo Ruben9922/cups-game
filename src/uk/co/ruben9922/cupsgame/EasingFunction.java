7b39e0fc4aa3e6ea49be94088d9947d5352df69b
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-09-25 22:28:09 +0100

    Change to use `@FunctionalInterface` annotation
    Add `@FunctionalInterface` annotation to functional interfaces
    `EasingFunction` and `VoidNullaryFunction`.

--- src/uk/co/ruben9922/cupsgame/EasingFunction.java
+++ src/uk/co/ruben9922/cupsgame/EasingFunction.java
@@ -1,5 +1,6 @@
 package uk.co.ruben9922.cupsgame;
 
+@FunctionalInterface
 interface EasingFunction {
     float getValue(int time, float beginning, float change, int duration);
 }
--- src/uk/co/ruben9922/cupsgame/VoidNullaryFunction.java
+++ src/uk/co/ruben9922/cupsgame/VoidNullaryFunction.java
@@ -1,5 +1,6 @@
 package uk.co.ruben9922.cupsgame;
 
+@FunctionalInterface
 interface VoidNullaryFunction {
     void run();
 }
