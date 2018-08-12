e93e88e36c837be54e91d55719816cbd01a7a33b
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-08-12 11:27:19 +0100

    Minor change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 100;
+camera.position.setZ(100);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
